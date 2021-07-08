ff5627c317ffe6a43d6245adc33ce46d8fc5613b
Eidson Sá <eidsonsa@MacBook-Air-de-Eidson.local>
2021-07-08 19:13:38 -0300

    boneco se movimentando

---
+++ Assets/MovimentComponent.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentComponent : MonoBehaviour
+{
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speed);
+    }
+}
